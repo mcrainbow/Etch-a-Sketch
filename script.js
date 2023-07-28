1e18666277d4065f3b1517390973ccb282206e04
Stephan Frolov <stefro97@yandex.ru>
2023-07-28 20:00:02 +0300

    Clear and Toggle Buttons Ready

--- script.js
+++ script.js
@@ -3,10 +3,14 @@
 // ! Some Variables
 const DEFAULT_SIZE = 8;
 const GAME_TYPE = "fill";
+const DEFAULT_BG = "rgb(211 211 211)";
 
 const gridField = document.querySelector(".grid__field"); // ! Grid Container
 const rangeText = document.querySelector(".boxes-text");
 const rangeInput = document.querySelector(".boxes-range");
+const clearBtn = document.querySelector(".clear-grid");
+const toggleGridBtn = document.querySelector(".toggle-grid");
+let gridBlocks = document.querySelectorAll(".grid__field-block");
 
 // ! Functions
 
@@ -24,6 +28,7 @@ const setGridTemplate = (gridContainer, value = DEFAULT_SIZE) => {
     block.classList.add("grid__field-block");
     gridContainer.appendChild(block);
   }
+  gridBlocks = document.querySelectorAll(".grid__field-block");
 };
 
 window.onload = setGridTemplate(gridField); // * Drawing blocks on load site
@@ -33,9 +38,49 @@ const reloadGrid = (value) => {
   clearGridTemplate(gridField);
   setGridTemplate(gridField, value);
 };
+const clearBg = (blocks) => {
+  for (let block of blocks) {
+    block.style.background = DEFAULT_BG;
+  }
+};
+const removeGridLines = (blocks) => {
+  for (let block of blocks) {
+    block.classList.add("noBorder");
+  }
+};
+const addGridLines = (blocks) => {
+  for (let block of blocks) {
+    block.classList.remove("noBorder");
+  }
+};
+const resetingToggleBtn = () => {
+  toggleGridBtn.value = "OFF";
+  toggleGridBtn.classList.remove("active");
+};
 
 // ! Event Listeners
 
+// * Range Input
 rangeInput.addEventListener("input", (e) => {
+  resetingToggleBtn();
   reloadGrid(e.target.value);
+  gridBlocks = document.querySelectorAll(".grid__field-block");
+});
+
+// * Clear button
+clearBtn.addEventListener("click", () => {
+  clearBg(gridBlocks);
+});
+
+// * Toggle button
+toggleGridBtn.addEventListener("click", (e) => {
+  if (e.target.value === "OFF") {
+    removeGridLines(gridBlocks);
+    e.target.value = "ON";
+    e.target.classList.add("active");
+  } else {
+    addGridLines(gridBlocks);
+    e.target.value = "OFF";
+    e.target.classList.remove("active");
+  }
 });
