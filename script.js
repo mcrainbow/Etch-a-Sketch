6cd4367451f29f73576f36f3344c37ebb17a5a78
Stephan Frolov <stefro97@yandex.ru>
2023-07-28 21:46:09 +0300

    First day final commit

--- script.js
+++ script.js
@@ -3,44 +3,31 @@
 // ! Some Variables
 const DEFAULT_SIZE = 8;
 const GAME_TYPE = "fill";
-const DEFAULT_BG = "rgb(211 211 211)";
+const DEFAULT_BG = "#d3d3d3";
+const DEFAULT_COLOR = "#333333";
+
+let currentDrawingMode = "";
+let currentDrawingColor = "";
 
 const gridField = document.querySelector(".grid__field"); // ! Grid Container
 const rangeText = document.querySelector(".boxes-text");
 const rangeInput = document.querySelector(".boxes-range");
 const clearBtn = document.querySelector(".clear-grid");
 const toggleGridBtn = document.querySelector(".toggle-grid");
+const modeButtons = document.querySelectorAll(".mode-btn");
+const bgPlate = document.querySelector(".bg-color");
+const penPlate = document.querySelector(".pen-color");
 let gridBlocks = document.querySelectorAll(".grid__field-block");
 
 // ! Functions
-
-const clearGridTemplate = (gridContainer) => {
-  gridContainer.innerHTML = "";
-};
-const setGridTemplate = (gridContainer, value = DEFAULT_SIZE) => {
-  gridContainer.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
-  gridContainer.style.gridTemplateRows = `repeat(${value}, 1fr)`;
-  rangeText.textContent = `Grid Size: ${value} x ${value}`;
-  rangeInput.value = value;
-
-  for (let i = 0; i < value * value; i++) {
-    let block = document.createElement("div");
-    block.classList.add("grid__field-block");
-    gridContainer.appendChild(block);
-  }
-  gridBlocks = document.querySelectorAll(".grid__field-block");
-};
-
-window.onload = setGridTemplate(gridField); // * Drawing blocks on load site
-
 const reloadGrid = (value) => {
   rangeText.textContent = `Grid Size: ${value} x ${value}`;
   clearGridTemplate(gridField);
   setGridTemplate(gridField, value);
 };
-const clearBg = (blocks) => {
+const clearBg = (blocks, color = DEFAULT_BG) => {
   for (let block of blocks) {
-    block.style.background = DEFAULT_BG;
+    block.style.background = color;
   }
 };
 const removeGridLines = (blocks) => {
@@ -57,6 +44,71 @@ const resetingToggleBtn = () => {
   toggleGridBtn.value = "OFF";
   toggleGridBtn.classList.remove("active");
 };
+const addingActiveClass = (btn) => {
+  modeButtons.forEach((button) => {
+    button.classList.remove("active");
+  });
+  btn.classList.add("active");
+};
+const gettingCurrentMode = () => {
+  let currentMode = "";
+  modeButtons.forEach((btn) => {
+    if (btn.classList.contains("active")) {
+      currentMode = btn.textContent;
+    }
+  });
+  return currentMode;
+};
+const gettingBGColor = (plate) => {
+  return plate.value;
+};
+const gettingPenColor = (plate) => {
+  return plate.value;
+};
+
+const colorMode = (elem) => {
+  currentDrawingColor = gettingPenColor(penPlate);
+  elem.style.background = currentDrawingColor;
+};
+const rainbowMode = (elem) => {
+  const randomR = Math.floor(Math.random() * 256);
+  const randomG = Math.floor(Math.random() * 256);
+  const randomB = Math.floor(Math.random() * 256);
+  currentDrawingColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  elem.style.background = currentDrawingColor;
+};
+
+const checkingForMode = (mode, blocks) => {
+  blocks.forEach((block) => {
+    block.addEventListener("click", () => {
+      console.log(mode);
+      if (mode === "Color Mode") {
+        colorMode(block);
+      } else if (mode === "Rainbow Mode") {
+        rainbowMode(block);
+      }
+    });
+  });
+};
+const clearGridTemplate = (gridContainer) => {
+  gridContainer.innerHTML = "";
+};
+const setGridTemplate = (gridContainer, value = DEFAULT_SIZE) => {
+  gridContainer.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${value}, 1fr)`;
+  rangeText.textContent = `Grid Size: ${value} x ${value}`;
+  rangeInput.value = value;
+
+  for (let i = 0; i < value * value; i++) {
+    let block = document.createElement("div");
+    block.classList.add("grid__field-block");
+    gridContainer.appendChild(block);
+  }
+  gridBlocks = document.querySelectorAll(".grid__field-block");
+  checkingForMode(currentDrawingMode, gridBlocks);
+};
+
+window.onload = setGridTemplate(gridField); // * Drawing blocks on load site
 
 // ! Event Listeners
 
@@ -65,11 +117,14 @@ rangeInput.addEventListener("input", (e) => {
   resetingToggleBtn();
   reloadGrid(e.target.value);
   gridBlocks = document.querySelectorAll(".grid__field-block");
+  clearBg(gridBlocks, gettingBGColor(bgPlate));
+  currentDrawingMode = gettingCurrentMode();
+  checkingForMode(currentDrawingMode, gridBlocks);
 });
 
 // * Clear button
 clearBtn.addEventListener("click", () => {
-  clearBg(gridBlocks);
+  clearBg(gridBlocks, gettingBGColor(bgPlate));
 });
 
 // * Toggle button
@@ -84,3 +139,29 @@ toggleGridBtn.addEventListener("click", (e) => {
     e.target.classList.remove("active");
   }
 });
+// * Mode Buttons
+modeButtons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    addingActiveClass(e.target);
+    currentDrawingMode = gettingCurrentMode();
+    console.log(currentDrawingMode);
+  });
+});
+
+// * Mode Drawing
+// gridBlocks.forEach((block) => {
+//   block.addEventListener("click", () => {
+//     console.log(currentDrawingMode);
+//     if (currentDrawingMode === "Color Mode") {
+//       colorMode(block);
+//     } else if (currentDrawingMode === "Rainbow Mode") {
+//       rainbowMode(block);
+//     }
+//   });
+// });
+
+// ! Реализовать:
+// ! 1 закрашивание по задержки кнопки мыши
+// ! 2 все режимы
+// ! 3 починить закраску при запкмке первой сетки
+// ! 4 сохранение цвета при нажатии (Color grabber)
