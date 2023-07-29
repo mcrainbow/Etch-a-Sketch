6173d54b30bd9f4867af7ee7af41ac22de40d454
Stephan Frolov <stefro97@yandex.ru>
2023-07-29 10:02:03 +0300

    Strating Grid Fully Fixed

--- script.js
+++ script.js
@@ -14,9 +14,13 @@ const rangeText = document.querySelector(".boxes-text");
 const rangeInput = document.querySelector(".boxes-range");
 const clearBtn = document.querySelector(".clear-grid");
 const toggleGridBtn = document.querySelector(".toggle-grid");
+
 const modeButtons = document.querySelectorAll(".mode-btn");
+const colorModeBtn = document.querySelector(".color-mode");
+
 const bgPlate = document.querySelector(".bg-color");
 const penPlate = document.querySelector(".pen-color");
+const grabberColor = document.querySelector(".color-grabber");
 let gridBlocks = document.querySelectorAll(".grid__field-block");
 
 // ! Functions
@@ -51,13 +55,11 @@ const addingActiveClass = (btn) => {
   btn.classList.add("active");
 };
 const gettingCurrentMode = () => {
-  let currentMode = "";
   modeButtons.forEach((btn) => {
     if (btn.classList.contains("active")) {
-      currentMode = btn.textContent;
+      currentDrawingMode = btn.textContent;
     }
   });
-  return currentMode;
 };
 const gettingBGColor = (plate) => {
   return plate.value;
@@ -78,18 +80,6 @@ const rainbowMode = (elem) => {
   elem.style.background = currentDrawingColor;
 };
 
-const checkingForMode = (mode, blocks) => {
-  blocks.forEach((block) => {
-    block.addEventListener("click", () => {
-      console.log(mode);
-      if (mode === "Color Mode") {
-        colorMode(block);
-      } else if (mode === "Rainbow Mode") {
-        rainbowMode(block);
-      }
-    });
-  });
-};
 const clearGridTemplate = (gridContainer) => {
   gridContainer.innerHTML = "";
 };
@@ -101,11 +91,55 @@ const setGridTemplate = (gridContainer, value = DEFAULT_SIZE) => {
 
   for (let i = 0; i < value * value; i++) {
     let block = document.createElement("div");
+    block.style.background = DEFAULT_BG;
     block.classList.add("grid__field-block");
     gridContainer.appendChild(block);
   }
   gridBlocks = document.querySelectorAll(".grid__field-block");
-  checkingForMode(currentDrawingMode, gridBlocks);
+};
+const drawingMode = (mode) => {
+  gridBlocks.forEach((block) => {
+    block.addEventListener("click", () => {
+      console.log(currentDrawingMode);
+      if (mode === "Color Mode" && colorModeBtn.classList.contains("active")) {
+        colorMode(block);
+      } else if (mode === "Rainbow Mode") {
+        rainbowMode(block);
+      }
+    });
+  });
+};
+
+function rgbToHex(r, g, b) {
+  return "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
+}
+const formatingRGB = (elem) => {
+  let rgbColor = elem.style.background;
+  let grbArr = rgbColor
+    .slice(4, rgbColor.length - 1)
+    .replaceAll(", ", " ")
+    .split(" ")
+    .map((num) => parseInt(num));
+  console.log("grbArr " + grbArr);
+  console.log("rgbToHex(...grbArr) " + rgbToHex(...grbArr));
+  return rgbToHex(...grbArr);
+};
+const pickingColor = (grid) => {
+  let color = "";
+  grid.forEach((elem) => {
+    elem.addEventListener("click", () => {
+      color = formatingRGB(elem);
+      penPlate.value = color;
+    });
+  });
+};
+
+const disablingModeBtns = (btns) => {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+    currentDrawingMode = "";
+    console.log(currentDrawingMode);
+  });
 };
 
 window.onload = setGridTemplate(gridField); // * Drawing blocks on load site
@@ -118,8 +152,8 @@ rangeInput.addEventListener("input", (e) => {
   reloadGrid(e.target.value);
   gridBlocks = document.querySelectorAll(".grid__field-block");
   clearBg(gridBlocks, gettingBGColor(bgPlate));
-  currentDrawingMode = gettingCurrentMode();
-  checkingForMode(currentDrawingMode, gridBlocks);
+  gettingCurrentMode();
+  drawingMode(currentDrawingMode);
 });
 
 // * Clear button
@@ -143,25 +177,30 @@ toggleGridBtn.addEventListener("click", (e) => {
 modeButtons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     addingActiveClass(e.target);
-    currentDrawingMode = gettingCurrentMode();
+    gettingCurrentMode();
     console.log(currentDrawingMode);
+    drawingMode(currentDrawingMode);
   });
 });
-
-// * Mode Drawing
-// gridBlocks.forEach((block) => {
-//   block.addEventListener("click", () => {
-//     console.log(currentDrawingMode);
-//     if (currentDrawingMode === "Color Mode") {
-//       colorMode(block);
-//     } else if (currentDrawingMode === "Rainbow Mode") {
-//       rainbowMode(block);
-//     }
-//   });
-// });
+// * Grabber Color Btn
+grabberColor.addEventListener("click", (e) => {
+  if (e.target.value === "OFF") {
+    e.target.value = "ON";
+    e.target.classList.add("active");
+    pickingColor(gridBlocks);
+    disablingModeBtns(modeButtons);
+  } else {
+    addGridLines(gridBlocks);
+    e.target.value = "OFF";
+    e.target.classList.remove("active");
+  }
+});
 
 // ! Реализовать:
 // ! 1 закрашивание по задержки кнопки мыши
 // ! 2 все режимы
-// ! 3 починить закраску при запкмке первой сетки
+// * 3 починить закраску при запкмке первой сетки
 // ! 4 сохранение цвета при нажатии (Color grabber)
+
+// При нажатии на сборку цвета происходит смена цвета в рэйнбоу моде
+// Сборка цвета работает безприрывно после первого включения
